bd56f538c8d3cad90f980ec2e6100c66efbc613a
Harald Pretl <harald.pretl@jku.at>
2023-11-02 22:47:01 +0100

    Update layver.sh

--- layver.sh
+++ layver.sh
@@ -3,38 +3,46 @@
 # layver.sh layout verification script
 # (c) 2023 Harald Pretl, IIC@JKU
 
+echo "[INFO] Layout verification script for SKY130_SAR-ADC1"
+
 RESDIR=$PWD/layver.tmp
 [ -d "$RESDIR" ] && rm -rf "$RESDIR"
 mkdir -p "$RESDIR"
 ERROR=0
 
-if false; then
+if true; then
 	CELL=adc_comp_latch
-	iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
-	iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
+	echo "[INFO] Checking cell $CELL"
+	./iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
+	./iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
 fi
 
-if false; then
+if true; then
 	CELL=adc_array_matrix_12bit
-	iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
-	iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
+	echo "[INFO] Checking cell $CELL"
+	./iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
+	./iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
 fi
 
-if false; then
+if true; then
 	CELL=adc_vcm_generator
-	iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
-	iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
+	echo "[INFO] Checking cell $CELL"
+	./iic-drc.sh -w "$RESDIR" -b mag/$CELL.mag || ERROR=1
+	./iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l mag/$CELL.mag -c $CELL || ERROR=1
 fi
 
 if true; then
 	CELL=sky130_mm_sc_hd_dlyPoly5ns
-	iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l stdcells/sky130_mm_sc_hd_dlyPoly5ns/$CELL.mag -c $CELL || ERROR=1
-	iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l stdcells/sky130_mm_sc_hd_dlyPoly5ns/$CELL.gds -c $CELL || ERROR=1
+	echo "[INFO] Checking cell $CELL"
+	./iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l stdcells/sky130_mm_sc_hd_dlyPoly5ns/$CELL.mag -c $CELL || ERROR=1
+	./iic-lvs.sh -w "$RESDIR" -s xschem/$CELL.sch -l stdcells/sky130_mm_sc_hd_dlyPoly5ns/$CELL.gds -c $CELL || ERROR=1
 fi
 
-if false; then
+if true; then
 	CELL=adc_top
-	iic-drc.sh -w "$RESDIR" -b gds/adc_top.gds.gz || ERROR=1
+	echo "[INFO] Checking cell $CELL"
+	./iic-drc.sh -w "$RESDIR" -b gds/$CELL.gds.gz || ERROR=1
+	./iic-lvs.sh -w "$RESDIR" -s lvs/$CELL.spice -l gds/$CELL.gds.gz -c $CELL || ERROR=1
 fi
 
 if [ $ERROR -eq 1 ]; then
